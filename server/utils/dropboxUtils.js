e721623177d55721021d186603912588e1226261
Indranil0603 <indranilsen1983@gmail.com>
2024-05-14 00:32:41 +0530

    changed the dropbox utilities

--- server/utils/dropboxUtils.js
+++ server/utils/dropboxUtils.js
@@ -1,39 +1,54 @@
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 const dropboxV2Api = require('dropbox-v2-api');
+
 const dropbox = dropboxV2Api.authenticate({
-    token: 'token'
+    token: process.env.DROPBOX_ACCESS_TOKEN
 });
 
-const sharedFolderLink = 'https://www.dropbox.com/sh/nx3tnilzqz7df8a/AAAYlTq2tiEHl5hsESw6-yfLa?e=1&dl=0';
+const sharedFolderLink = 'https://www.dropbox.com/sh/nx3tnilzqz7df8a/AAAYlTq2tiEHl5hsESw6-yfLa';
 
-const sharedFolderId = sharedFolderLink.match(/\/sh\/(.*)\//)[1];
-console.log(sharedFolderId)
-
-function downloadFilesFromFolder(folderPath) {
+function downloadFilesFromFolder(folderPath, localFolderPath) {
     dropbox({
-        resource: 'files/download',
+        resource: 'files/list_folder',
         parameters: {
-            path: `/${sharedFolderId}${folderPath}` 
+            shared_link: { url: sharedFolderLink },
+            path: folderPath
         }
     }, (err, result, response) => {
         if (err) {
-            return console.error(`Error downloading files from folder '${folderPath}':`, err);
+            return console.error(`Error listing files in folder '${folderPath}':`, err);
         }
-        result.entries.forEach(file => {
-            
+        console.log(response.body);
+        const files = result.entries.filter(entry => entry['.tag'] === 'file');
+
+        files.forEach(file => {
+            const downloadPath = file.id // Construct download path using folderPath and file name
+            const localFilePath = path.join(localFolderPath, file.name);
+
             dropbox({
                 resource: 'files/download',
                 parameters: {
-                    path: file.path_display
-                },
-                download: true
+                    path: downloadPath
+                }
             }, (err, result, response) => {
                 if (err) {
-                    return console.error('Error downloading file:', err);
+                    console.error(`Error downloading file '${downloadPath}':`, err);
+                 
+                } else {
+                    const writer = fs.createWriteStream(localFilePath);
+                    result.pipe(writer);
+                    console.log(`Downloaded file: ${localFilePath}`);
                 }
-                console.log(`Downloaded file: ${file.name}`);
             });
         });
     });
 }
 
-downloadFilesFromFolder('/atcoder_testcases/2019ddccqual/A/in');
+const localFolderPath = './input'; // Specify the local folder path
+if (!fs.existsSync(localFolderPath)) {
+    fs.mkdirSync(localFolderPath);
+}
+
+downloadFilesFromFolder('/2019ddccqual/A/in', localFolderPath);
